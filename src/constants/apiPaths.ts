af29153d71d242c32d8b637c699262b4ef385740
Aliaksandr Nisunou <Aliaksandr_Nisunou@epam.com>
2023-04-09 12:15:35 +0300

    Integrate createProduct lambda with FE

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://cyx09zl7v6.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://avosomxem8.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://cyx09zl7v6.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://avosomxem8.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
